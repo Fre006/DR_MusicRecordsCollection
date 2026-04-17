abe8aa3aaf9a32ff4bded286ad936ad9ca4275f7
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2026-04-17 10:30:11 +0200

    Tests for GetBy methods

--- DR_MusicRecordsCollection/Repos/Interfaces/IRecordsRepo.cs
+++ DR_MusicRecordsCollection/Repos/Interfaces/IRecordsRepo.cs
@@ -5,5 +5,8 @@ namespace DR_MusicRecordsCollection.Repos.Interfaces
     public interface IRecordsRepo
     {
         public IEnumerable<Record> GetAll();
+        public IEnumerable<Record> GetByTitle(string title);
+        public IEnumerable<Record> GetByArtist(string artist);
+        public IEnumerable<Record> GetByPublicationYear(int pubYear);
     }
 }
